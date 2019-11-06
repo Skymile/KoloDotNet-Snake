290cc4f84a922b0d346c0893b9028700acb86018
Patryk Milewski <mpatryk97@gmail.com>
2019-11-06 22:46:09 +0100

    Zajęcia 2

--- DotNetSnake/DotNetSnake/Program.cs
+++ DotNetSnake/DotNetSnake/Program.cs
@@ -7,10 +7,47 @@ namespace DotNetSnake
 	class Program
 	{
 		private static int size = 8;
-		
+
+		static void Test()
+		{
+			int[] tab = new int[4];
+			int[] bb =
+			{
+				1, 2, 3, 4
+			};
+			int[][] tab1 = new int[4][];
+			tab1[0] = new int[5] { 1, 2, 3, 4, 5 };
+			int[,,,,,,,,,] bg;
+
+			var list = new List<int>();
+
+			var queue = new Queue<int>();
+			// 123
+			// 23
+			// 234
+
+			var stack = new Stack<int>();
+			// 123
+			// 23
+			// 423
+
+			var linkedlist = new LinkedList<int>();
+			// 1 <-> 2 <-> 3 <-> 4 <-> 5 <-> 6
+
+			var dict = new Dictionary<string, int>();
+			dict.Add("asda", 8);
+			dict.Add("asdd", 8);
+			dict.Add("asde", 8);
+
+			dict["asda"] = 4;
+
+
+		}
+
 		static void Main(string[] args)
 		{
 			var q = new Queue<int>();
+
 
 			q.Enqueue(1);
 			q.Enqueue(2);
@@ -18,10 +55,25 @@ namespace DotNetSnake
 
 			Display(q);
 
-			char c = Console.ReadKey().KeyChar;
+			char c = char.ToUpper(Console.ReadKey().KeyChar);
 
-			;
+			switch (c)
+			{
+				case 'W':
+					q.Dequeue();
+					q.Enqueue(4);
+					break;
+				case 'S':
 
+					break;
+				case 'A':
+
+					break;
+				case 'D':
+
+					break;
+			}
+			Display(q);
 
 		}
 
