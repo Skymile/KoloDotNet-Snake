8f641cc313b9691dbd4915f1f8155ca471739223
Patryk Milewski <mpatryk97@gmail.com>
2020-01-15 18:16:03 +0100

    Books, authors

--- DotNetSnake/Functional/Program.cs
+++ DotNetSnake/Functional/Program.cs
@@ -18,18 +18,70 @@ namespace Functional
 		}
 	}
 
+	class RefType
+	{
+		public int Property { get; set; }
+	}
+
+
+	// Ref Types
+	// Value Types
+
+	// Pass-by-value
+	// Pass-by-reference
+	class Author
+	{
+		public int    Id   { get; set; }
+		public string Name { get; set; }
+	}
+
+	class Book
+	{
+		public int    Id    { get; set; }
+		public string Title { get; set; }
+	}
+
+	class BookAuthor
+	{
+		public int AuthorId { get; set; }
+		public int BookId   { get; set; }
+	}
+
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			foreach (int i in 
-					Randomizer
-						.GetRandomNumbers(16, 10, 0)
-						.Where(i => i % 2 == 0)
-				)
+			Author[] authors =
 			{
-				Console.WriteLine(i);
-			}
+				new Author { Id = 1, Name = "Author1" },
+				new Author { Id = 2, Name = "Author2" },
+				new Author { Id = 3, Name = "Author3" },
+			};
+
+			Book[] books =
+			{
+				new Book { Id = 14, Title = "14 Book" },
+				new Book { Id = 15, Title = "15 Book" },
+				new Book { Id = 16, Title = "16 Book" },
+				new Book { Id = 17, Title = "17 Book" },
+				new Book { Id = 18, Title = "18 Book" },
+				new Book { Id = 19, Title = "19 Book" },
+				new Book { Id = 20, Title = "20 Book" },
+				new Book { Id = 21, Title = "21 Book" },
+			};
+
+			BookAuthor[] bookAuthors =
+			{
+				new BookAuthor { AuthorId = 1, BookId = 14 },
+				new BookAuthor { AuthorId = 1, BookId = 15 },
+				new BookAuthor { AuthorId = 1, BookId = 16 },
+				new BookAuthor { AuthorId = 2, BookId = 17 },
+				new BookAuthor { AuthorId = 2, BookId = 19 },
+			};
+
+
+
 			Console.ReadLine();
 		}
 	}
